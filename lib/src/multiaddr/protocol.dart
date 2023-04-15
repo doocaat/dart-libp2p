00f88775474d7ea559317851722d583d3fa9a74e
Stanyslav Glubyshev <sglubyshev@iponweb.net>
2023-04-15 22:55:23 +0300

    refactor protocol multiaddr

--- lib/src/multiaddr/protocol.dart
+++ lib/src/multiaddr/protocol.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:dart_multihash/src/multihash/varint_utils.dart';
-import 'package:fixnum/fixnum.dart';
-import '../multibase/multibase.dart';
 import 'protocol_map.dart';
-import 'package:bs58/bs58.dart' as bs;
 import 'package:buffer/buffer.dart';
 
 class Protocol {
@@ -38,7 +34,6 @@ class Protocol {
       }
       buf[i] = x;
       return i + 1;
-      return decodeVarint(buf, x).numBytesRead;
   }
 
   static Protocol byName(String name) {
@@ -69,172 +64,12 @@ class Protocol {
     if(addr.isEmpty){
       return Uint8List(0);
     }
-    try {
-      switch (_type) {
-        case ProtocolType.IP4:
-          {
-            try {
-              var address = InternetAddress(addr);
-              if (address.type == InternetAddressType.IPv4) {
-                return address.rawAddress;
-              }
-              throw Exception("Invalid IPv4 address: $addr");
-            } catch (e) {
-              throw FormatException("Invalid IPv4 address: $addr");
-            }
-          }
 
-        case ProtocolType.IP6:
-          try {
-            var address = InternetAddress(addr);
-            if (address.type == InternetAddressType.IPv6) {
-              return address.rawAddress;
-            }
-            throw Exception("Invalid IPv6 address: $addr");
-          } catch (e) {
-            throw FormatException("Invalid IPv6 address: $addr");
-          }
-        case ProtocolType.IP6ZONE:
-          if (addr.isEmpty) {
-            throw FormatException("Empty IPv6 zone!");
-          }
-
-          if (addr.contains("/")) {
-            throw FormatException("IPv6 zone ID contains '/'");
-          }
-
-          return Uint8List.fromList(utf8.encode(addr));
-        case ProtocolType.TCP:
-        case ProtocolType.UDP:
-        case ProtocolType.DCCP:
-        case ProtocolType.SCTP:
-          {
-            int x = int.parse(addr);
-            if (x > 65535 || x < 0) {
-              throw FormatException(
-                  "Failed to parse ${_type.name} address 0 > $addr > 65535");
-            }
-
-            var bb = BytesBuilder();
-            bb.addByte(x >> 8);
-            bb.addByte(x);
-            return bb.toBytes();
-          }
-        case ProtocolType.P2P:
-        case ProtocolType.IPFS:
-          {
-            if (!addr.startsWith("Qm") && !addr.startsWith("1")) {
-              throw FormatException("Incorrect hash, should star on Qm or 1");
-            }
-            try {
-              var bytes = bs.base58.decode(addr);
-              if (bytes.length < 32 || bytes.length > 50) {
-                throw FormatException("Invalid hash length: ${bytes.length}");
-              }
-              return bytes;
-            } catch (e) {
-              throw FormatException("Incorrect hash: $addr");
-            }
-          }
-        case ProtocolType.ONION3:
-          {
-            List<String> split = addr.split(":");
-            if (split.length != 2) {
-              throw FormatException("Onion3 address needs a port: $addr");
-            }
-
-            // onion3 address without the ".onion" substring
-            if (split[0].length != 56) {
-              throw FormatException(
-                  "failed to parse $name addr: $addr not a Tor onion3 address.");
-            }
-
-            Uint8List onionHostBytes = multibaseDecode("b${split[0]}");
-            if (onionHostBytes.length != 35) {
-              throw FormatException("Invalid onion3 address host: ${split[0]}");
-            }
-
-            int port = int.parse(split[1]);
-            if (port > 65535) {
-              throw FormatException("Port is > 65535: $port");
-            }
-
-            if (port < 1) {
-              throw FormatException("Port is < 1: $port");
-            }
-
-            BytesBuilder dout = BytesBuilder();
-            dout.add(onionHostBytes);
-            dout.addByte(port >> 8);
-            dout.addByte(port);
-            return dout.toBytes();
-          }
-        case ProtocolType.UNIX:
-          {
-            if (addr.startsWith("/")) {
-              addr = addr.substring(1);
-            }
-            Uint8List path = Uint8List.fromList(utf8.encode(addr));
-            BytesBuilder dout = BytesBuilder();
-            Uint8List length =
-                Uint8List((((32 - Int32(path.length).numberOfLeadingZeros() + 6) / 7).floor()));
-            putUvarint(length, path.length);
-            dout.add(length);
-            dout.add(path);
-            return Uint8List.fromList(utf8.encode(addr));
-          }
-        case ProtocolType.DNS:
-        case ProtocolType.DNS4:
-        case ProtocolType.DNS6:
-        case ProtocolType.DNSADDR:
-            return Uint8List.fromList(utf8.encode(addr));
-
-        default:
-          throw FormatException("Unknown multiaddr type");
-      }
-    } catch (e) {
-      throw FormatException(e.toString());
-    }
+    return _type.serializer(this, addr);
   }
 
-
-
   String readAddress(Uint8List addr) {
-    int size = sizeForAddress(addr);
-    switch (_type) {
-      case ProtocolType.IP4:
-        var buf = read(addr, size);
-        return InternetAddress.fromRawAddress(buf).address;
-      case ProtocolType.IP6:
-        var buf = read(addr, size);
-        return InternetAddress.fromRawAddress(buf).address;
-      case ProtocolType.IP6ZONE:
-        return utf8.decode(addr);
-      case ProtocolType.TCP:
-      case ProtocolType.UDP:
-      case ProtocolType.DCCP:
-      case ProtocolType.SCTP: {
-        return ((addr[0] << 8) + addr[1]).toString();
-      }
-      case ProtocolType.P2P:
-      case ProtocolType.IPFS:
-        return bs.base58.encode(addr);
-      case ProtocolType.ONION3: {
-        var host = Uint8List.fromList(addr.getRange(0, 35).toList());
-        var portBytes = Uint8List.fromList(addr.getRange(35, addr.length).toList());
-        var port = ((portBytes[0] << 8) + portBytes[1]);
-        return "${multibaseEncode(Multibase.base32, host).substring(1)}:$port";
-      }
-      case ProtocolType.UNIX: {
-        return utf8.decode(addr);
-      }
-      case ProtocolType.DNS:
-      case ProtocolType.DNS4:
-      case ProtocolType.DNS6:
-      case ProtocolType.DNSADDR:
-        return utf8.decode(addr);
-    }
-    throw FormatException("Unimplemented protocol type: ${_type.name}");
+    return _type.deserializer(this, addr);
   }
 
   Uint8List read(Uint8List source, int len) {
@@ -281,8 +116,3 @@ class Protocol {
   @override
   String toString() => _type.toString();
 }
-
-
-
-Uint8List _int32bytes(int value) =>
-    Uint8List(4)..buffer.asInt32List()[0] = value;
--- lib/src/multiaddr/protocol_map.dart
+++ lib/src/multiaddr/protocol_map.dart
@@ -1,6 +1,12 @@
 import 'dart:collection';
+import 'dart:io';
 import 'dart:typed_data';
 import 'protocol.dart';
+import 'package:bs58/bs58.dart' as bs;
+import 'dart:convert' as convert;
+import '../multibase/multibase.dart';
+import 'package:fixnum/fixnum.dart';
+
 
 class ProtocolMap {
   final _nameMap = HashMap<String, Protocol>();
@@ -35,43 +41,195 @@ class ProtocolMap {
 const int lengthPrefixedVarSize = -1;
 
 enum ProtocolType {
-  IP4(4, 32, "ip4"),
-  TCP(6, 16, "tcp"),
-  DCCP(33, 16, "dccp"),
-  IP6(41, 128, "ip6"),
-  IP6ZONE(42, lengthPrefixedVarSize, "ip6zone"),
-  IPCIDR(43, 8, "ipcidr"),
-  DNS(53, lengthPrefixedVarSize, "dns"),
-  DNS4(54, lengthPrefixedVarSize, "dns4"),
-  DNS6(55, lengthPrefixedVarSize, "dns6"),
-  DNSADDR(56, lengthPrefixedVarSize, "dnsaddr"),
-  SCTP(132, 16, "sctp"),
-  UDP(273, 16, "udp"),
-  P2PWEBRTC(276, 0, "p2p-webrtc-direct"),
-  UTP(301, 0, "utp"),
-  UDT(302, 0, "udt"),
-  UNIX(400, lengthPrefixedVarSize, "unix"),
-  P2P(421, lengthPrefixedVarSize, "p2p"),
-  IPFS(421, lengthPrefixedVarSize, "ipfs"),
-  HTTPS(443, 0, "https"),
-  ONION3(445, 296, "onion3"),
-  TLS(448, 0, "tls"),
-  NOISE(454, 0, "noise"),
-  QUIC(460, 0, "quic"),
-  QUIC_V1(461, 0, "quic-v1"),
-  WEBTRANSPORT(465, 0, "webtransport"),
-  CERTHASH(466, lengthPrefixedVarSize, "certhash"),
-  WS(477, 0, "ws"),
-  WSS(478, 0, "wss"),
-  P2PCIRCUIT(290, 0, "p2p-circuit"),
-  HTTP(480, 0, "http");
+  IP4(4, 32, "ip4", _Serializer.ipV4, _Deserializer.ipV4),
+  TCP(6, 16, "tcp", _Serializer.uint16, _Deserializer.uint16),
+  DCCP(33, 16, "dccp", _Serializer.uint16, _Deserializer.uint16),
+  IP6(41, 128, "ip6", _Serializer.ipV6, _Deserializer.ipV6),
+  IP6ZONE(42, lengthPrefixedVarSize, "ip6zone", _Serializer.utf8, _Deserializer.utf8),
+  DNS(53, lengthPrefixedVarSize, "dns", _Serializer.utf8, _Deserializer.utf8),
+  DNS4(54, lengthPrefixedVarSize, "dns4", _Serializer.utf8, _Deserializer.utf8),
+  DNS6(55, lengthPrefixedVarSize, "dns6", _Serializer.utf8, _Deserializer.utf8),
+  DNSADDR(56, lengthPrefixedVarSize, "dnsaddr", _Serializer.utf8, _Deserializer.utf8),
+  SCTP(132, 16, "sctp", _Serializer.uint16, _Deserializer.uint16),
+  UDP(273, 16, "udp", _Serializer.uint16, _Deserializer.uint16),
+  P2PWEBRTC(276, 0, "p2p-webrtc-direct", _Serializer.noImplement, _Deserializer.noImplement),
+  UTP(301, 0, "utp", _Serializer.noImplement, _Deserializer.noImplement),
+  UDT(302, 0, "udt", _Serializer.noImplement, _Deserializer.noImplement),
+  UNIX(400, lengthPrefixedVarSize, "unix", _Serializer.unix, _Deserializer.utf8),
+  P2P(421, lengthPrefixedVarSize, "p2p", _Serializer.base58, _Deserializer.base58),
+  IPFS(421, lengthPrefixedVarSize, "ipfs", _Serializer.base58, _Deserializer.base58),
+  HTTPS(443, 0, "https", _Serializer.noImplement, _Deserializer.noImplement),
+  ONION3(445, 296, "onion3", _Serializer.onion3, _Deserializer.onion3),
+  TLS(448, 0, "tls", _Serializer.noImplement, _Deserializer.noImplement),
+  NOISE(454, 0, "noise", _Serializer.noImplement, _Deserializer.noImplement),
+  QUIC(460, 0, "quic", _Serializer.noImplement, _Deserializer.noImplement),
+  QUIC_V1(461, 0, "quic-v1", _Serializer.noImplement, _Deserializer.noImplement),
+  WEBTRANSPORT(465, 0, "webtransport", _Serializer.noImplement, _Deserializer.noImplement),
+  CERTHASH(466, lengthPrefixedVarSize, "certhash", _Serializer.noImplement, _Deserializer.noImplement),
+  WS(477, 0, "ws", _Serializer.noImplement, _Deserializer.noImplement),
+  WSS(478, 0, "wss", _Serializer.noImplement, _Deserializer.noImplement),
+  P2PCIRCUIT(290, 0, "p2p-circuit", _Serializer.noImplement, _Deserializer.noImplement),
+  HTTP(480, 0, "http", _Serializer.noImplement, _Deserializer.noImplement);
 
-  const ProtocolType(this.code, this.size, this.name);
+  const ProtocolType(this.code, this.size, this.name, this.serializer, this.deserializer);
 
   final int code;
   final int size;
   final String name;
+  final Uint8List Function(Protocol, String) serializer;
+  final String Function(Protocol, Uint8List) deserializer;
 }
 
-Uint8List _int32bytes(int value) =>
-    Uint8List(4)..buffer.asInt32List()[0] = value;
+class _Serializer {
+
+  static Uint8List ipV4(Protocol _, String addr) {
+    {
+      try {
+        var address = InternetAddress(addr);
+        if (address.type == InternetAddressType.IPv4) {
+          return address.rawAddress;
+        }
+        throw Exception("Invalid IPv4 address: $addr");
+      } catch (e) {
+        throw FormatException("Invalid IPv4 address: $addr");
+      }
+    }
+  }
+
+  static Uint8List ipV6(Protocol _, String addr) {
+    try {
+      var address = InternetAddress(addr);
+      if (address.type == InternetAddressType.IPv6) {
+        return address.rawAddress;
+      }
+      throw Exception("Invalid IPv6 address: $addr");
+    } catch (e) {
+      throw FormatException("Invalid IPv6 address: $addr");
+    }
+  }
+
+  static Uint8List uint16(Protocol protocol, String addr) {
+    int x = int.parse(addr);
+    if (x > 65535 || x < 0) {
+      throw FormatException(
+          "Failed to parse ${protocol.name} address 0 > $addr > 65535");
+    }
+
+    var bb = BytesBuilder();
+    bb.addByte(x >> 8);
+    bb.addByte(x);
+
+    return bb.toBytes();
+  }
+
+  static Uint8List base58(Protocol _, String addr) {
+    if (!addr.startsWith("Qm") && !addr.startsWith("1")) {
+      throw FormatException("Incorrect hash, should star on Qm or 1");
+    }
+    try {
+      var bytes = bs.base58.decode(addr);
+      if (bytes.length < 32 || bytes.length > 50) {
+        throw FormatException("Invalid hash length: ${bytes.length}");
+      }
+      return bytes;
+    } catch (e) {
+      throw FormatException("Incorrect hash: $addr");
+    }
+  }
+
+  static Uint8List utf8(Protocol _, String addr) {
+    return Uint8List.fromList(convert.utf8.encode(addr));
+  }
+
+  static Uint8List unix(Protocol _, String addr) {
+    if (addr.startsWith("/")) {
+      addr = addr.substring(1);
+    }
+    Uint8List path = Uint8List.fromList(convert.utf8.encode(addr));
+    BytesBuilder dout = BytesBuilder();
+    Uint8List length =
+    Uint8List((((32 - Int32(path.length).numberOfLeadingZeros() + 6) / 7).floor()));
+    Protocol.putUvarint(length, path.length);
+    dout.add(length);
+    dout.add(path);
+    return Uint8List.fromList(convert.utf8.encode(addr));
+  }
+
+  static Uint8List onion3(Protocol protocol, String addr) {
+    List<String> split = addr.split(":");
+    if (split.length != 2) {
+      throw FormatException("Onion3 address needs a port: $addr");
+    }
+
+    // onion3 address without the ".onion" substring
+    if (split[0].length != 56) {
+      throw FormatException(
+          "failed to parse ${protocol.name} addr: $addr not a Tor onion3 address.");
+    }
+
+    Uint8List onionHostBytes = multibaseDecode("b${split[0]}");
+    if (onionHostBytes.length != 35) {
+      throw FormatException("Invalid onion3 address host: ${split[0]}");
+    }
+
+    int port = int.parse(split[1]);
+    if (port > 65535) {
+      throw FormatException("Port is > 65535: $port");
+    }
+
+    if (port < 1) {
+      throw FormatException("Port is < 1: $port");
+    }
+
+    var bb = BytesBuilder()
+      ..add(onionHostBytes)
+      ..addByte(port >> 8)
+      ..addByte(port);
+    return bb.toBytes();
+  }
+
+  static Uint8List noImplement(Protocol protocol, String _) {
+    throw Exception("No value serializer for protocol $protocol");
+  }
+}
+
+class _Deserializer {
+  static String ipV4(Protocol _, Uint8List addr) {
+    try {
+      return InternetAddress.fromRawAddress(addr).address;
+    } catch (e) {
+      throw FormatException("Invalid IPv4 address: $addr");
+    }
+  }
+
+  static String ipV6(Protocol _, Uint8List addr) {
+    try {
+      return InternetAddress.fromRawAddress(addr).address;
+    } catch (e) {
+      throw FormatException("Invalid IPv6 address: $addr");
+    }
+  }
+
+  static String uint16(Protocol _, Uint8List addr) {
+    return ((addr[0] << 8) + addr[1]).toString();
+  }
+
+  static String base58(Protocol _, Uint8List addr) {
+    return bs.base58.encode(addr);
+  }
+
+  static String utf8(Protocol _, Uint8List addr) {
+    return convert.utf8.decode(addr);
+  }
+
+  static String onion3(Protocol _, Uint8List addr) {
+    var host = Uint8List.fromList(addr.getRange(0, 35).toList());
+    var portBytes = Uint8List.fromList(addr.getRange(35, addr.length).toList());
+    var port = ((portBytes[0] << 8) + portBytes[1]);
+    return "${multibaseEncode(Multibase.base32, host).substring(1)}:$port";
+  }
+
+  static String noImplement(Protocol protocol, Uint8List _) {
+    throw Exception("No value deserializer for protocol $protocol");
+  }
+}
